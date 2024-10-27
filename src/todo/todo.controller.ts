8722e88ec53deca3732c32d55468eb4173075eb8
mohamad1ghasemi <mohammad1ghasemi1@gmail.com>
2024-10-27 21:18:20 +0330

    add code to todo files

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ITodoService } from './interface/todo-service.interface';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: ITodoService) {}
+
+  @Get()
+  findAll() {
+    return this.todoService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.todoService.findOne(id);
+  }
+  @Post()
+  create(@Body('title') title: string) {
+    return this.todoService.create(title);
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body('isCompleted') isCompleted: boolean) {
+    return this.todoService.update(id, isCompleted);
+  }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.todoService.delete(id);
+  }
+}
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
 import { TodoService } from './todo.service';
+import { ITodoService } from './interface/todo-service.interface';
+import { ITodoRepository } from './interface/todo-repository.interface';
+import { TodoRepository } from './todo.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [
+    { provide: ITodoService, useClass: TodoService },
+    { provide: ITodoRepository, useClass: TodoRepository },
+  ],
 })
 export class TodoModule {}
